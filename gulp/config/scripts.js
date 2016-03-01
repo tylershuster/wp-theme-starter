c68a8425bb9b44177c124ee23a0ba983063064aa
Tyler Shuster <tyler@shuster.house>
2016-03-01 10:55:08 -0800

    Update scripts.js

--- gulp/config/scripts.js
+++ gulp/config/scripts.js
@@ -1,7 +1,18 @@
+var webpack = require('webpack');
 /**
  * Override the Default
  * Core Scripts
  * Config
  *
  */
-module.exports = {};
+module.exports = {
+  options: {
+    webpack: {
+      defaults: {
+        externals: {
+          jquery: 'window.jQuery'
+        }
+      }
+    }
+  }
+};
