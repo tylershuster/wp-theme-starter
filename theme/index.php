f0f89260f65bf950ce3d5edb8b68e5264bb259a9
Tyler Shuster <tyler@shuster.house>
2017-08-01 13:10:37 -0700

    Update index.php

--- theme/index.php
+++ theme/index.php
@@ -8,7 +8,10 @@
 <?php get_header(); ?>
 
 	<?php while(have_posts()): the_post(); ?>
-		<main><?php echo get_the_content(); ?></main>
+		<main>
+			<h1><?php the_title(); ?></h1>
+			<?php the_content(); ?>
+		</main>
 	<?php endwhile; ?>
 
 <?php get_footer(); ?>
